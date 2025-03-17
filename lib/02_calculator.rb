c0b5cfb33fc54fd6a8578a38b739af8fb7243cee
Sasa Stojiljkovic <sasa.stojiljkovic1992@gmail.com>
2025-03-17 20:35:59 +0200

    Version without factoriel

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,8 +1,5 @@
-
-
-
 puts "All the maths is in my hand! What I can do for you?"
-print "Enter the first number:    "
+print "Enter the first number: "
 number1 = gets.chomp.to_i
 print "Now enter the second number: "
 number2 = gets.chomp.to_i
@@ -10,6 +7,7 @@ number2 = gets.chomp.to_i
 def add(number1, number2)
   a = number1 + number2
   puts "This is your results for addition #{a}"
+  return a
 end
 
 add(number1, number2)
@@ -17,27 +15,24 @@ add(number1, number2)
 def subtract(number1, number2)
   b = number1 - number2
   puts "This is your results for subtract #{b}"
+  return b
 end
 
 subtract(number1, number2)
 
-def sum(array)
-  return array.sum 
+def sum(number1, number2)
+  array = [number1, number2]
+  results = array.sum
+  puts "The sum of numbers in array are: #{number1} and #{number2} is: #{results}"
+  return results
 end
 
-user_input = [number1, number2]
-results = sum(user_input)
-
-puts "The sum of numbers in array are: #{number1} and #{number2} is: #{results}"
-puts "The next examples are there for testing purpose for other numbers in array:"
-puts sum([0, 0])
-puts sum([7])
-puts sum([7, 11])
-puts sum([1,3,5,7,9])
+sum(number1, number2)
 
 def multiply(number5, number6)
   c = number5 * number6
   puts "This is your results for multiplication #{c}"
+  return c
 end
 
 multiply(number1, number2)
@@ -46,18 +41,22 @@ multiply(number1, number2)
 def power (primary, exponent)
   d = primary ** exponent
   puts "This is your results for powering #{d}"
+  return d
 end
 
 power(number1, number2)
 
 
-
-# # def factorial(number)
-  
-# # puts factorial(0)
-# # puts factorial(1)
-# # puts factorial(2)
-# # puts factorial(5)
-# # puts factorial(10)
+# def factorial(value)
+#   return 1 if value == 0 || value == 1
+#   (1..value).reduce(:*)
+#   return value
+# end 
+# factorial(number1, number2)
+# puts factorial(0)
+# puts factorial(1)
+# puts factorial(2)
+# puts factorial(5)
+# puts factorial(10)
 
 
